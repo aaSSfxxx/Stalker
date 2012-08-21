e76cc3edd354e7a626b0c0b4f3e94cc6e9226aa3
aaSSfxxx <aassfxxx@hackerzvoice.net>
2012-08-21 13:49:09 +0200

    fixing licence bugs

--- README.txt
+++ README.txt
@@ -1,9 +1,14 @@
 "THE BEER-WARE LICENSE" (Revision 42):
 <aassfxxx@hackerzvoice.net> wrote this file. As long as you retain this notice you
 can do whatever you want with this stuff. If we meet some day, and you think
-this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+this stuff is worth it, you can buy me a beer in return.
 ----------------------------------------------------------------------------------
 This file is part of Stalker (name suggested by Overclok[])
 
 Warning: this software is experimental (and will stay experimental), so you
-use it at your OWN risk.
+use it at your OWN risk (in a virtual machine for exemple, if you want to trace
+malwares with it). 
+
+TODO: 
+ - add hooks to NtCreateProcess to catch processes created by the tracked process
+ - maybe make a GUI ?
--- dll/trace.c
+++ dll/trace.c
@@ -3,7 +3,7 @@
  * "THE BEER-WARE LICENSE" (Revision 42):
  * <aassfxxx@hackerzvoice.net> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+ * this stuff is worth it, you can buy me a beer in return.
  * ----------------------------------------------------------------------------------
  * This file is part of Stalker (name suggested by Overclok[])
  *
@@ -23,13 +23,15 @@ int APIENTRY DllMain(HINSTANCE hInst, DWORD dwReason, LPVOID lpReserved) {
 			//Create the communication pipe
 			hFile = CreateFile("\\\\.\\pipe\\StalkerWPMTrack", GENERIC_READ | GENERIC_WRITE, 0 , NULL, OPEN_EXISTING, 0 , NULL);
 			ReadFile(hFile,&inf , sizeof(BOOTSTRAP_INFO), &dwRead, 0);
-			snprintf(buf, 1000, "%d - %d\n", inf.allowCall, inf.noResume); 
+			snprintf(buf, 1000, "%d - %d\n", inf.allowCall, inf.noResume);
+			
 			//Place hooks for WriteProcessMemory
 			krnl32 = GetModuleHandle("kernel32.dll");
 			lpWriteProcMem = (LPWRITEPROCESSMEM) GetProcAddress(krnl32, "WriteProcessMemory");
 			placeHook(handleWriteProcessMemory, GetProcAddress(krnl32, "WriteProcessMemory"), inf.executableBase);
 			placeEATHooking(handleWriteProcessMemory, "WriteProcessMemory", krnl32);
-			//Place hooks for ResumeThread
+			
+			//Place hooks for ResumeThread if asked
 			if(inf.noResume == TRUE) {
 				placeHook(handleResumeThread, GetProcAddress(krnl32, "ResumeThread"), inf.executableBase);
 				placeEATHooking(handleResumeThread, "ResumeThread", krnl32);
